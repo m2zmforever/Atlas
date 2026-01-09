979fcdb9b90f18b679b402d0b8c334d2f6050b37
zm <m2zmforever@gmail.com>
2026-01-09 14:48:14 +0300

    Create Load

---
+++ Load.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/m2zmforever/Atlas/refs/heads/main/Script.lua", true))()
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/m2zmforever/Atlas/refs/heads/main/Script.lua"))()
