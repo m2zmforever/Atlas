9b81be05b034aef50a66c8212c4b4bcc22684441
zm <m2zmforever@gmail.com>
2026-02-08 06:35:01 +0300

    Delete Load

--- Load.lua
+++
@@ -1,2 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/m2zmforever/Atlas/Release/Script.lua", true))()
-loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/m2zmforever/Atlas/Release/Script.lua"))()
